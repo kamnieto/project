2a04648af1166dbaf590a8ba80a0bbc715cde8ba
kamnieto <andrexnieto@gmail.com>
2023-04-10 22:43:00 -0500

    adicion del menu en la version movil

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const iconMovileMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-navEmail.addEventListener('click',viewToggle);
+navEmail.addEventListener('click',ToggleDesktopMenu);
+iconMovileMenu.addEventListener('click',ToggleMobileMenu);
 
-function viewToggle(){
+function ToggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+function ToggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
